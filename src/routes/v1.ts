1c4a93ad00ad78964607730873032c5013bb5938
yaeoni <tjghwnd103@gmail.com>
2021-09-14 22:00:02 +0900

    [CHORE] route 수정 (version 반영)

--- src/routes/v1.ts
+++ src/routes/v1.ts
@@ -7,10 +7,10 @@ import getToken from "../controller/getToken";
 const router = express.Router();
 
 // 토큰 발급
-router.get("/get", getToken);
+router.get("/v1/getToken", getToken);
 
 // 유효 토큰 검사
-router.get("/verify", verifyToken, (req: Request, res: Response) => {
+router.get("/v1/verifyToken", verifyToken, (req: Request, res: Response) => {
   res.json(req["decoded"]);
 });
 
